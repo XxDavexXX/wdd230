750dc23825fcf8368cd320cb00ed13e79043fe37
XxDavexX <dave.santivanez@gmail.com>
2024-03-28 21:11:10 -0500

    avance

--- scripts/links.js
+++ scripts/links.js
@@ -4,7 +4,41 @@ const linksURL = "https://raw.githubusercontent.com/XxDavexXX/wdd230/main/data/l
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
-  }
-  
-  getLinks();
+    displayLinks(data.weeks);
+}
+
+function displayLinks(weeksData) {
+    const weekList = document.getElementById('weekList');
+
+    weeksData.forEach(weekData => {
+        const li = document.createElement('li');
+        const div = document.createElement('div');
+        div.textContent = weekData.week + ':';
+        li.appendChild(div);
+
+        let lastIndex = weekData.links.length - 1; // Obtener el índice del último enlace
+        weekData.links.forEach((link, index) => {
+            const a = document.createElement('a');
+            a.href = baseURL + '/' + link.url; // Agregar el baseURL al enlace
+            a.textContent = link.title;
+            li.appendChild(a);
+            
+            // Agregar un separador si no es el último enlace
+            if (index !== lastIndex) {
+                const separator = document.createTextNode(' | ');
+                li.appendChild(separator);
+            }
+        });
+
+        weekList.appendChild(li);
+    });
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    getLinks();
+});
+
+
+
+
