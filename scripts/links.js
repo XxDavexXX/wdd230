df77be58c4765bad1622db84ae1eb9862dde0c3b
XxDavexX <dave.santivanez@gmail.com>
2024-03-28 20:48:02 -0500

    avance

--- scripts/links.js
+++ scripts/links.js
@@ -1,2 +1,10 @@
 const baseURL = "https://github.com/XxDavexXX/wdd230";
-const linksURL = "https://github.com/XxDavexXX/wdd230/tree/main/scripts/data/links.json";
+const linksURL = "https://github.com/XxDavexXX/wdd230/blob/main/data/links.json";
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    console.log(data);
+  }
+  
+  getLinks();
